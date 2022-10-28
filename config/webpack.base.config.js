3bf3d75ca1e5834709514c3b50411d7c71237aaa
华炳淘 <huabingtao@huazhu.com>
2022-10-28 19:03:48 +0800

    first commit

---
+++ config/webpack.base.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const { resolve } = require('../utils');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackDevServer = require('webpack-dev-server');
+
+module.exports = {
+  entry: path.resolve(__dirname,'..','client/index.js'),
+  module: {
+    rules: [
+      {
+          test: /\.(js|jsx)$/,
+          // 只编译app文件夹下的文件
+          include: path.resolve(__dirname, '..','client'),
+          use: {
+              loader: 'babel-loader',
+              options: {
+                  presets: [
+                      '@babel/preset-env',
+                      '@babel/preset-react',
+                  ],
+              }
+          }
+      },
+    ]
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, '..','dist'),
+    clean: true
+  },
+  resolve: {
+    // 设置路径别名
+    alias: {
+        '@': path.resolve(__dirname, '..', 'client'),
+    },
+    // 文件后缀自动补全, 就是你import文件的时候如果没写后缀名就会优先找下面这几个
+    extensions: [ '.js', '.jsx' ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: path.resolve(__dirname, '..', 'public/index.html')
+    })
+  ]
+};
