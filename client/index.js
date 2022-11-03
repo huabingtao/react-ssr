de1d0657d69cf3254e13ea414ef7736171553791
华炳淘 <huabingtao@huazhu.com>
2022-11-03 16:33:48 +0800

    feat: add router

--- client/index.js
+++ client/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './reset.less'
 import './index.less'
-import Live2D from  './assets/Live2D.png'
+import App from './App'
+import { BrowserRouter } from "react-router-dom";
 
-function App() {
-    return <div>
-      <h1>Hello React1</h1>
-      <div className='fontsize'>fontSize</div>
-      <img src={Live2D} width="200px"></img>
-    </div>
-}
-createRoot(document.getElementById('app')).render(<App />)
-// render(<App/>, document.getElementById('app'));
+
+createRoot(document.getElementById('app')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+)
--- config/webpack.production.config.js
+++ config/webpack.production.config.js
@@ -40,7 +40,9 @@ module.exports = merge(baseConfig, {
       minimizer: [
         // 在 webpack@5 中，你可以使用 `...` 语法来扩展现有的 minimizer（即 `terser-webpack-plugin`），将下一行取消注释
         `...`,
-        new CssMinimizerPlugin(),
+        new CssMinimizerPlugin({
+          exclude: /[\\/]node_modules[\\/]/,
+        }),
       ],
     },
 })
