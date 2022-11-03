ffa589113ffcdba1dd62ff4fb957069a1770d887
华炳淘 <huabingtao@huazhu.com>
2022-11-03 11:08:39 +0800

    feat: add css minimizer

--- config/webpack.production.config.js
+++ config/webpack.production.config.js
@@ -1,6 +1,7 @@
 const { merge } = require('webpack-merge');
 const webpack = require('webpack');
 const baseConfig = require('./webpack.base.config.js');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = merge(baseConfig, {
     devtool: 'hidden-nosources-cheap-source-map',
@@ -36,5 +37,10 @@ module.exports = merge(baseConfig, {
           },
         },
       },
+      minimizer: [
+        // 在 webpack@5 中，你可以使用 `...` 语法来扩展现有的 minimizer（即 `terser-webpack-plugin`），将下一行取消注释
+        `...`,
+        new CssMinimizerPlugin(),
+      ],
     },
 })
