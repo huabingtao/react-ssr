f5f687249c52196f0e48dc35de54908bf25e10d5
华炳淘 <huabingtao@huazhu.com>
2022-11-04 18:37:40 +0800

    feat: add authProvider

---
+++ client/components/authProvider.js
@@ -0,0 +1,40 @@
+
+import * as React from 'react'
+const fakeAuthProvider = {
+  isAuthenticated: false,
+  signin(callback) {
+    fakeAuthProvider.isAuthenticated = true;
+    setTimeout(callback, 100); // fake async
+  },
+  signout(callback) {
+    fakeAuthProvider.isAuthenticated = false;
+    setTimeout(callback, 100);
+  },
+};
+
+
+
+
+function AuthProvider({ children }) {
+  let [user, setUser] = React.useState(null);
+  let AuthContext = React.createContext(null);
+  let signin = (newUser, callback) => {
+    return fakeAuthProvider.signin(() => {
+      setUser(newUser);
+      callback();
+    });
+  };
+
+  let signout = (callback) => {
+    return fakeAuthProvider.signout(() => {
+      setUser(null);
+      callback();
+    });
+  };
+
+  let value = { user, signin, signout };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
+
+export default AuthProvider
--- client/components/layout.js
+++ client/components/layout.js
@@ -1,21 +1,24 @@
 import * as React from "react";
 import { Outlet, Link } from "react-router-dom";
+import AuthProvider from "./authProvider";
 
-export default function(){
+export default function () {
   return (<>
-        <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/nothing-here">Nothing Here</Link>
-          </li>
-        </ul>
-      </nav>
-  <Outlet></Outlet>
-  </>) 
+    <AuthProvider>
+      <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/nothing-here">Nothing Here</Link>
+            </li>
+          </ul>
+        </nav>
+        <Outlet></Outlet>
+    </AuthProvider>    
+  </>)
 }
