fba1348595f5426ef3dc5c4679150a568db839bf
华炳淘 <huabingtao@huazhu.com>
2022-11-04 16:54:34 +0800

    feat: split route

---
+++ client/components/layout.js
@@ -0,0 +1,21 @@
+import * as React from "react";
+import { Outlet, Link } from "react-router-dom";
+
+export default function(){
+  return (<>
+        <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/nothing-here">Nothing Here</Link>
+          </li>
+        </ul>
+      </nav>
+  <Outlet></Outlet>
+  </>) 
+}
